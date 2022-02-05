387541e7c1861bf3eaafb8b2ff2844f7f2f436ff
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-05 12:38:31 +0900

    Update 1018.py

--- 1018.py
+++ 1018.py
@@ -1,7 +1,7 @@
 C=[];v='WBWBWBWB'
 for _ in range(4):C+=[v,v[::-1]]
 a,b=map(int,input().split());L=[]
-for y in range(a):L.append(input())
+for y in range(a):L+=[input()]
 l=[]
 for j in range(a-7):
  for i in range(b-7):
@@ -9,5 +9,5 @@ for j in range(a-7):
   for q in range(8):
    for p in range(8):
     if L[j+q][i+p]==C[q][p]:c+=1
-  l.append(32-abs(c-32))
+  l+=[32-abs(c-32)]
 print(min(l))
