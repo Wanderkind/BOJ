fe31f1935c8451db81b70aa4656cfecf4be8d630
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-05 17:33:29 +0900

    Create 18111.py

---
+++ 18111.py
@@ -0,0 +1,11 @@
+n,m,b=map(int,input().split());L=[];T=[]
+for _ in range(n):L+=list(map(int,input().split()))
+for i in range(max(L),min(L)-1,-1):
+ c=b;t=0
+ for j in range(m*n):
+  x=i-L[j];c-=x
+  if 0<=x:t+=x
+  else:t-=2*x
+ if c<0:t=9**9
+ T+=[t]
+print(min(T),max(L)-T.index(min(T)))
