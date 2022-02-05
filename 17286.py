47e3f1522a281790fafd431fb8898f11428cf070
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-05 12:40:21 +0900

    Update 17286.py

--- 17286.py
+++ 17286.py
@@ -1,4 +1,4 @@
 l=[]
-for _ in range(4):l.append(list(map(int,input().split())))
+for _ in range(4):l+=[list(map(int,input().split()))]
 def d(a,b):return((l[a][0]-l[b][0])**2+(l[a][1]-l[b][1])**2)**(1/2)
 print(int(min([d(0,1)+d(1,2)+d(2,3),d(0,1)+d(1,3)+d(2,3),d(0,2)+d(1,2)+d(1,3),d(0,2)+d(2,3)+d(1,3),d(3,0)+d(2,3)+d(1,2),d(3,0)+d(1,3)+d(1,2)])))
