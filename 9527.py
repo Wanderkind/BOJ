91847505b72b92a0cba8a1f95fbdcaeeff45f538
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-07 17:19:03 +0900

    Create 9527.py

---
+++ 9527.py
@@ -0,0 +1,16 @@
+def f(n):
+ a=1
+ while n>1:a+=n%2;n//=2
+ return a if n>0 else 0
+N=[*map(int,input().split())];A=[]
+for i in range(2):
+ n=N[i];n-=int(i<1);l=[];a=k=1
+ if n!=0:
+  while n>1:
+   l+=[n%2];n//=2
+  for i in range(len(l)-1,-1,-1):
+   if l[i]==1:a=2*a+k+1;k=2*k+1
+   else:a+=a-f(k)+k;k*=2
+ else:a=0
+ A+=[a]
+print(A[1]-A[0])
