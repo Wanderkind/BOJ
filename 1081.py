690290fe8e34ee30dc249a0f69cd354ddb7117ce
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-07 18:07:06 +0900

    Create 1081.py

---
+++ 1081.py
@@ -0,0 +1,15 @@
+def f(n,j):
+ x=0
+ for k in range(0,len(str(n))):
+  d=(n//(10**k))%10;s=(n//(10**(k+1)))*(10**k)+n%(10**k)
+  if d>j:a=((s//(10**k))+1)*(10**k)
+  elif d==j:a=s+1
+  else:a=((s//(10**k)))*(10**k)
+  x+=a
+ return x
+X=0;l=list(map(int,input().split()))
+for q in range(2):
+ n=l[q];n+=q-1;L=0
+ for i in range(9):L+=f(n,i+1)*(i+1)
+ X-=L*(-1)**q
+print(X)
