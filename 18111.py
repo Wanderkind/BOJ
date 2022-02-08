6edf66746ea925d93a582aa1e6afcd0c96ec9213
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-08 11:10:11 +0900

    Update 18111.py

--- 18111.py
+++ 18111.py
@@ -4,8 +4,7 @@ for i in range(max(L),min(L)-1,-1):
  c=b;t=0
  for j in range(m*n):
   x=i-L[j];c-=x
-  if 0<=x:t+=x
-  else:t-=2*x
+  t+=x if 0<=x else -2*x
  if c<0:t=9**9
  T+=[t]
 print(min(T),max(L)-T.index(min(T)))
