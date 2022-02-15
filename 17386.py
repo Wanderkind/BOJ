d4dac4d3c227559d254bf322570f5aa19ffc8524
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-02-16 01:44:15 +0900

    Update 17386.py

--- 17386.py
+++ 17386.py
@@ -1,4 +1,4 @@
 a,b,c,d=map(int,input().split());p,q,r,s=map(int,input().split())
-if a==c or p==r:u=555/797;v=572/797;a=u*a-v*b;b=v*a+u*b;c=u*c-v*d;d=v*c+u*d;p=u*p-v*q;q=v*p+u*q;r=u*r-v*s;s=v*r+u*s
-try:x=((c-a)*(q*r-p*s)-(r-p)*(b*c-a*d))/((r-p)*(d-b)-(c-a)*(s-q));print(0 if(a-x)*(c-x)>0 or(p-x)*(r-x)>0 else 1)
+u=555/797;v=572/797;A=u*a-v*b;B=v*a+u*b;C=u*c-v*d;D=v*c+u*d;P=u*p-v*q;Q=v*p+u*q;R=u*r-v*s;S=v*r+u*s
+try:x=((C-A)*(Q*R-P*S)-(R-P)*(B*C-A*D))/((R-P)*(D-B)-(C-A)*(S-Q));print(0 if(A-x)*(C-x)>0 or(P-x)*(R-x)>0 else 1)
 except ZeroDivisionError:print(0)
