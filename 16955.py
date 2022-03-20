76830f54426ac40999cf82129b52013e93da8a10
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-03-20 13:54:53 +0900

    Update 16955.py

--- 16955.py
+++ 16955.py
@@ -4,17 +4,12 @@ def f(z):
  return int(r>0)
 e,l=0,[]
 for i in range(10):l+=[input()]
-for i in range(10):e+=f(l[i])
 for i in range(10):
  s=''
  for j in range(10):s+=l[j][i]
- e+=f(s)
+ e+=f(s)+f(l[i])
 for i in range(19):
- I,s=min(i,9),''
- for j in range(10-abs(i-9)):s+=l[I-j][i-I+j]
- e+=f(s)
-for i in range(19):
- I,s=max(9-i,0),''
- for j in range(10-abs(i-9)):s+=l[I+j][I+j+i-9]
- e+=f(s)
+ I,s,K,t=min(i,9),'',max(9-i,0),''
+ for j in range(10-abs(i-9)):s+=l[I-j][i-I+j];t+=l[K+j][K+j+i-9]
+ e+=f(s)+f(t)
 print(int(e>0))
