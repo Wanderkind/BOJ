dc3648af03f357b8dd61cff6198b08133567e2df
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-03-31 16:06:50 +0900

    Update 11390.py

--- 11390.py
+++ 11390.py
@@ -3,5 +3,5 @@ e,f,n,k=map(int,input().split())
 a,b=sorted([e,f])
 c=a**2+b**2
 A,B,s,i=a**2/c,b**2/c,1,-1
-while s<=k and i<n+1:i+=1;s+=comb(n,i)
+while s<=k:i+=1;s+=comb(n,i)
 print(log(a*b*A**i*B**(n-i)/2))
