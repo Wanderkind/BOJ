98245bcc9145900d0b2bf0f8a26ef31fe6a4aa04
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-04-03 12:40:19 +0900

    Update 6600.py

--- 6600.py
+++ 6600.py
@@ -1,7 +1,6 @@
-import math
-while 1:
-    try:
-        x1,y1,x2,y2,x3,y3=map(float,input().split())
-        print(abs(round(math.pi*math.sqrt((x1-x3)**2+(y1-y3)**2)/math.sin(math.pi-abs(math.atan2(y1-y2,x1-x2)-math.atan2(y3-y2,x3-x2)-math.pi)),2)))
-    except EOFError:
-        break
+from math import*
+try:
+ while 1:
+  A,a,B,b,C,c=map(float,input().split())
+  print(round(abs(pi*hypot(A-C,a-c)/sin(atan2(a-b,A-B)-atan2(c-b,C-B))),2))
+except EOFError:exit()
