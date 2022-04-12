fdd35b62332d75ec1e23da6d530c30cc61d99f8d
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-04-12 22:52:44 +0900

    Update 1986.py

--- 1986.py
+++ 1986.py
@@ -1,54 +1,18 @@
-l=[];z=[]
+l,z,M,I,T,R=[],[],map,int,input,range
 def u(x,y):return(k[2*i+1]+x,k[2*i+2]+y)
-n,m=map(int,input().split())
-q=list(map(int,input().split()))
-k=list(map(int,input().split()))
-p=list(map(int,input().split()))
-for i in range(p[0]):z+=[(p[2*i+1],p[2*i+2])]
-for i in range(k[0]):z+=[u(0,0)];l+=[u(1,2),u(2,1),u(-1,2),u(2,-1),u(1,-2),u(-2,1),u(-1,-2),u(-2,-1)]
-for i in range(q[0]):z+=[(q[2*i+1],q[2*i+2])]
-for i in range(q[0]):
- s=1;N=q[2*i+1];M=q[2*i+2]
- while N+s<=n:
-  o=(N+s,M)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while s<N:
-  o=(N-s,M)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while M+s<=m:
-  o=(N,M+s)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while s<M:
-  o=(N,M-s)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while N+s<=n and M+s<=m:
-  o=(N+s,M+s)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while N+s<=n and s<M:
-  o=(N+s,M-s)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while s<N and s<M:
-  o=(N-s,M-s)
-  if o not in z:l+=[o];s+=1
-  else:break
- s=1
- while s<n and M+s<=m:
-  o=(N-s,M+s)
-  if o not in z:l+=[o];s+=1
-  else:break
+def H(a,b,c,d,e,g,h,i,j):
+ s,L=1,[]
+ while a*M+b*N+c*s<d and e*M+g*s<h and(N+i*s,M+j*s)not in z:L+=[(N+i*s,M+j*s)];s+=1
+ return L
+n,m=M(I,T().split())
+q=[*M(I,T().split())]
+k=[*M(I,T().split())]
+p=[*M(I,T().split())]
+for i in R(p[0]):z+=[(p[2*i+1],p[2*i+2])]
+for i in R(k[0]):z+=[u(0,0)];l+=[u(1,2),u(2,1),u(-1,2),u(2,-1),u(1,-2),u(-2,1),u(-1,-2),u(-2,-1)]
+for i in R(q[0]):z+=[(q[2*i+1],q[2*i+2])]
+for i in R(q[0]):N,M=q[2*i+1],q[2*i+2];l+=[*H(0,1,1,n+1,0,0,1,1,0),*H(0,0,1,N,0,0,1,-1,0),*H(1,0,1,m+1,0,0,1,0,1),*H(0,0,1,M,0,0,1,0,-1),*H(0,1,1,n+1,1,1,m+1,1,1),*H(0,1,1,n+1,0,1,M,1,-1),*H(0,0,1,N,0,1,M,-1,-1),*H(0,0,1,n,1,1,m+1,-1,1)]
 l+=z
-for i in range(len(l)-1,-1,-1):
+for i in R(len(l)-1,-1,-1):
  if not(0<l[i][0]<=n and 0<l[i][1]<=m):del l[i]
 print(n*m-len(list(set(l))))
