878065b446263036acd16b3fd14fcbed1f2716b4
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-04-13 17:54:51 +0900

    Update 7869.py

--- 7869.py
+++ 7869.py
@@ -1,8 +1,7 @@
 from math import*
 X,Y,R,x,y,r=map(float,input().split())
 d=hypot(X-x,Y-y)
-if R+r<d:exit(print('0.000'))
+if R+r<d:k=0
 elif abs(R-r)<d:a=(d**2+R**2-r**2)/2/d;k=R**2*acos(a/R)+r**2*acos((d-a)/r)-d*sqrt(R**2-a**2)
 else:k=(min(R,r)**2)*pi
-v=str(round(k,3))
-print(v+'0'*(4+v.index('.')-len(v)))
+print('{:.3f}'.format(round(k,3)))
