ac61519543ac51f735bfba24d48b65423b7bdc6c
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-04-28 01:02:48 +0900

    Update 18111.py

--- 18111.py
+++ 18111.py
@@ -2,9 +2,7 @@ n,m,b=map(int,input().split());L=[];T=[]
 for _ in range(n):L+=[*map(int,input().split())]
 for i in range(max(L),min(L)-1,-1):
  c=b;t=0
- for j in range(m*n):
-  x=i-L[j];c-=x
-  t-=2*x if 0>x else -x
+ for j in range(m*n):x=i-L[j];c-=x;t-=x*(3*(0>x)-1)
  if c<0:t=9**9
  T+=[t]
 print(min(T),max(L)-T.index(min(T)))
