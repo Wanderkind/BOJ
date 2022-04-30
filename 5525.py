a35a80cc5756f0ab980f96ab224bfbc46b2a72e7
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-01 04:13:01 +0900

    Update 5525.py

--- 5525.py
+++ 5525.py
@@ -1,14 +1,7 @@
-a=int(input())
-B=int(input())
-b=B//2+2
-S=input()
-l=[]
-y=-3
-for i in range(B-2):
-    if S[i]+S[i+1]+S[i+2]=='IOI':
-        if y==i-2:
-            l[-1]+=1
-        else:
-            l.append(1)
-        y=i
-print(sum([(i-a+1)*(i>=a)for i in l]))
+a,b,S,l,y=int(input()),int(input()),input(),[],-3
+for i in range(b-2):
+ if S[i]+S[i+1]+S[i+2]=='IOI':
+  if y==i-2:l[-1]+=1
+  else:l.append(2)
+  y=i
+print(sum([(i-a)*(i>a)for i in l]))
