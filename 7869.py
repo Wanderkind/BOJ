4d0c84286aa718a0663471c5dd7545a6f344ea74
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-01 11:16:45 +0900

    Update 7869.py

--- 7869.py
+++ 7869.py
@@ -2,6 +2,6 @@ from math import*
 X,Y,R,x,y,r=map(float,input().split())
 d=hypot(X-x,Y-y)
 if R+r<d:k=0
-elif abs(R-r)<d:a=(d**2+R**2-r**2)/2/d;k=R**2*acos(a/R)+r**2*acos((d-a)/r)-d*sqrt(R**2-a**2)
+elif abs(R-r)<d:a=(d*d+R*R-r*r)/2/d;k=R*R*acos(a/R)+r*r*acos((d-a)/r)-d*sqrt(R*R-a*a)
 else:k=(min(R,r)**2)*pi
 print('{:.3f}'.format(round(k,3)))
