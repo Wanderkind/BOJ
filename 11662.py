f9584cdbdad323f1ceabd85c5c3ddeab19128fb3
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-07 14:29:17 +0900

    Update 11662.py

--- 11662.py
+++ 11662.py
@@ -1,8 +1,8 @@
 A,a,B,b,C,c,D,d=map(int,input().split())
 p,r=A-C,a-c
 q,s=B-D-p,b-d-r
-k=q**2+s**2
-if k==0:print((p**2+r**2)**.5);exit()
+k=q*q+s*s
+if k==0:exit(print((p*p+r*r)**.5))
 def f(t):return((p+q*t)**2+(r+s*t)**2)**.5
 u=-(p*q+r*s)/k
-print(f(u if 0<u<1 else int(1<u)))
+print(f(u if 0<u<1 else 1<u))
