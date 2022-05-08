a826472999c828cd3104ef7c6a633b430b07d487
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-08 17:26:49 +0900

    Update 21133.py

--- 21133.py
+++ 21133.py
@@ -1,5 +1,5 @@
 n=int(input())
-a,b=list(range(2,n+1,2)),list(range(1,n+1,2))
+a,b=[*range(2,n+1,2)],[*range(1,n+1,2)]
 if n%6==2:del b[2];b+=[5];del b[1];b=[3]+b
 if n%6==3:del a[0];a+=[2];del b[1];del b[0];b+=[1,3]
 for i in a+b:print(i)
