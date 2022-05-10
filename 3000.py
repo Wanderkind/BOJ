8ba88a48063fb7edadf91dad816b1344c5b5683b
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-10 12:24:29 +0900

    Update 3000.py

--- 3000.py
+++ 3000.py
@@ -1,8 +1,8 @@
 X,Y={},{}
 for _ in range(int(input())):
  x,y=map(int,input().split())
- if x in X:X[x].append(y)
+ if x in X:X[x]+=[y]
  else:X[x]=[y]
- if y not in Y:Y[y]=0
- else:Y[y]+=1
+ if y in Y:Y[y]+=1
+ else:Y[y]=0
 print(sum(sum(Y[j]for j in X[i])*(len(X[i])-1)for i in X))
