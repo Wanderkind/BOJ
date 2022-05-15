393360f3752e5b7219a86cf1c5e4c6e24aeaaf37
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-15 15:42:55 +0900

    Update 1341.py

--- 1341.py
+++ 1341.py
@@ -1,4 +1,4 @@
 a,b=map(int,input().split())
 z=1
 while(2**z-1)%b*(z<61):z+=1
-print(''.join(['-*'[int(i)]for i in bin(a*(2**z-1)//b)[2:].zfill(z)])if z<61 else-1)
+print([-1,''.join(['-*'[int(i)]for i in bin(a*(2**z-1)//b)[2:].zfill(z)])][z<61])
