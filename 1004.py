efd4eff52092edd3e8de66dd7c4713c52bd79210
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-17 22:11:07 +0900

    Update 1004.py

--- 1004.py
+++ 1004.py
@@ -1,6 +1,6 @@
-for i in range(int(input())):
- a=0;b,c,d,e=map(int,input().split());n=int(input())
- for j in range(n):
-  p,q,r=map(int,input().split())
-  if((b-p)**2+(c-q)**2-r**2)*((d-p)**2+(e-q)**2-r**2)<0:a+=1
+I=input
+U=lambda:map(int,I().split())
+for i in range(int(I())):
+ a=0;b,c,d,e=U();n=int(I())
+ for j in range(n):p,q,r=U();a+=((b-p)**2+(c-q)**2-r*r)*((d-p)**2+(e-q)**2-r*r)<0
  print(a)
