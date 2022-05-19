f66a7b319d8da4a28f143272da6f7a69d25d58c3
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-19 22:41:02 +0900

    Update 1419.py

--- 1419.py
+++ 1419.py
@@ -1,16 +1,4 @@
-import math
-l=int(input());r=int(input());k=int(input())
-if k%2==0:k//=2
-x=r//k-math.ceil(l/k)+1
-if k==1:
- for i in range(2):
-  if l<=[1,2][i]<=r:x-=1 
-elif k==3:
- if l<=3<=r:x-=1
-elif k==2:
- for i in range(5):
-  if l<=[2,4,6,8,12][i]<=r:x-=1
-else:
- for i in range(2):
-  if l<=[5,10][i]<=r:x-=1
-print(x)
+I=input;v=int
+l=v(I());r=v(I());k=v(I())
+k//=(2-k%2)
+print(r//k-v((l+k-.1)/k)+1-sum(l<=[[1,2],[2,4,6,8,12],[3],0,[5,10]][k-1][i]<=r for i in range([2,5,1,0,2][k-1])))
