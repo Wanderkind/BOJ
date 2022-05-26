4683918a7631d2d7770c53ac5a6692baeaf9c26a
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-26 23:31:55 +0900

    Update 16113.py

--- 16113.py
+++ 16113.py
@@ -7,6 +7,6 @@ for i in L:
  if'.'*5==i:l+=[[]]
  else:l[-1]+=[i]
 def h(z):return''.join(['#.'[i%2]*int(z[i])for i in R(len(z))])
-def f(z):return''.join(z)if z!=[]else''
+def f(z):return['',''.join(z)][z!=[]]
 def g(z):return str([*map(h,['636','5','114111411','111121116','34125','312111213','6111213','14145','61116','3121116'])].index(z))if z!=''else''
 print(''.join([*map(g,map(f,l))]))
