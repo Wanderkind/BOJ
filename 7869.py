f582621c63553f3d012041767cc69e1e6bc95103
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-06-03 10:04:22 +0900

    Update 7869.py

--- 7869.py
+++ 7869.py
@@ -4,4 +4,4 @@ d=hypot(X-x,Y-y)
 if R+r<d:k=0
 elif abs(R-r)<d:a=(d*d+R*R-r*r)/2/d;k=R*R*acos(a/R)+r*r*acos((d-a)/r)-d*sqrt(R*R-a*a)
 else:k=(min(R,r)**2)*pi
-print('{:.3f}'.format(round(k,3)))
+print('{:.3f}'.format(k))
