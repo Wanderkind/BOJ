69f8d87891841edb3a4bb23ff7c34ad08115aa3e
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-07-20 14:41:57 +0900

    Create 10886.c

---
+++ C Basics/10886.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	int a,b,c=0,i;
+	scanf("%d",&a);
+	for (i=0;i<a;i++){
+		scanf("%d",&b);c+=b;
+	}
+	printf(a/c>1?"Junhee is not cute!":"Junhee is cute!");
+	return 0;
+}
