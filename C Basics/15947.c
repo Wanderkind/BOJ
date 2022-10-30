10cd8b248b21f9cd7439ba38520888a2434089dd
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-10-31 00:33:57 +0900

    Create 15947.c

---
+++ C Basics/15947.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+//# include <math.h>
+//# define ll long long
+
+char l[14][10]={
+	"baby","sukhwan","tururu","turu",
+	"very","cute","tururu","turu",
+	"in","bed","tururu","turu",
+	"baby","sukhwan"
+};
+
+int main(){
+	
+	int n;
+	scanf("%d",&n);
+	n--;
+	
+	int a=n/14;
+	int b=n%14;
+	int c=b%4;
+	
+	if (c>1){
+		if (c-a>-1){
+			printf(l[b]);
+			int i=0;
+			for (;i<a;i++){
+				printf("ru");
+			}
+		}
+		else{
+			printf("tu+ru*%d",a-c+4);
+		}
+	}
+	else{
+		printf(l[b]);
+	}
+	
+	return 0;
+}
