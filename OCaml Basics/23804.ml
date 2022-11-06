8cedc786d938d43ef63b5a0523b4a876a49559b2
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-11-06 17:51:24 +0900

    Create 23804.ml

---
+++ OCaml Basics/23804.ml
@@ -0,0 +1,25 @@
+open Printf
+;;
+let n = read_int()
+;;
+for i=1 to n do
+    for j=1 to n do
+        printf "@@@@@"
+    done;
+    printf "\n"
+done
+;;
+for i=1 to 3*n do
+    for j=1 to n do
+        printf "@"
+    done;
+    printf "\n"
+done
+;;
+for i=1 to n do
+    for j=1 to n do
+        printf "@@@@@"
+    done;
+    printf "\n"
+done
+;;
